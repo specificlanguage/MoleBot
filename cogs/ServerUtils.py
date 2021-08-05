af5cd160ce57ab1d479b642790a978319b52e734
specificlanguage <specificlanguage@gmail.com>
2021-08-05 19:49:08 -0400

    Added ServerUtils cog, server pinger feature

---
+++ cogs/ServerUtils.py
@@ -0,0 +1,44 @@
+import discord, datetime
+import main
+from mcstatus import MinecraftServer
+from discord.ext import commands
+from discord_slash import cog_ext, SlashCommand, SlashContext
+from discord_slash.utils.manage_commands import create_option
+
+config = main.CONFIG["ServerUtils"]
+
+class ServerUtils(commands.Cog, name="ServerUtils"):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @cog_ext.cog_slash(name="ping",
+                       description="Ping the default server or another server (note this cannot ping Bedrock servers)",
+                       guild_ids=main.CONFIG["guild_ids"],
+                       options=[create_option(name="server_ip",
+                                              description="Enter an ip different",
+                                              option_type=3,
+                                              required=False)])
+    async def ping(self, ctx: SlashContext, server_ip=config["default_server"]):
+        # This feels very tacky, may want to change this later
+        server = MinecraftServer.lookup(server_ip)
+        try:
+            status = server.status()
+            embed = discord.Embed(title=server_ip, color=discord.Color.dark_orange(),
+                                  description="Running {0}".format(status.version.name))
+            embed.add_field(name="Players Online",
+                            value="**{0}/{1}**".format(status.players.online, status.players.max))
+            embed.add_field(name="Latency", value="**{0}** ms".format(status.latency))
+            embed.add_field(name="MOTD", value=status.description)
+            embed.set_footer(text="Pinged at {0} UTC".format(datetime.datetime.utcnow().strftime("%m/%d/%Y, %H:%M:%S")))
+            # set image of icon (if there is one)
+            await ctx.send(embed=embed)
+        except:
+            await ctx.send("Could not find **" + server_ip + "**, it's either offline or non-existent.")
+
+    # other commands that will become part of this cog:
+    # whereis [x] [y] [z] to find a location in the world
+    # civmap to give a link to civmap
+
+
+def setup(bot):
+    bot.add_cog(ServerUtils(bot))
--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 import json
-from discord import Client, Intents, Embed, File
+from discord import Intents, Embed, File
+from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
+class Bot(commands.Bot):
+    def __init__(self, intents):
+        super().__init__(command_prefix="%", help_command=None, intents=intents)
 
 def load_config():
-    with open("config-example.json") as json_data_file:
+    with open("config.json") as json_data_file:
         data = json.load(json_data_file)
     return data
 
+CONFIG = load_config()
+bot = Bot(intents=Intents.default())
+slash = SlashCommand(bot, sync_commands=True)
+for extension in CONFIG["enabled_cogs"]:
+    bot.load_extension("cogs." + extension)
 
-config = load_config()
-client = Client(intents=Intents.default())
-slash = SlashCommand(client, sync_commands=True)
 
-
-@slash.slash(name="mole", description="Mole guy", guild_ids=[803472116061569065])
+@slash.slash(name="mole", description="Mole guy", guild_ids=CONFIG["guild_ids"])
 async def mole(ctx: SlashContext):
     await ctx.send(file=File('resources/montymole.gif'))
 
 
-@client.event
+
+@bot.event
 async def on_ready():
     print("Ready!")
 
 
-client.run(config["token"])
+bot.run(CONFIG["token"])
