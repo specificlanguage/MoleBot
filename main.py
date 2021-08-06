be2e01e61de61c39817707826a33f472ad445fed
specificlanguage <specificlanguage@gmail.com>
2021-08-05 20:51:28 -0400

    Added short delusional easter egg from previous bots
    Traditionally on Civ servers if you say "delusional" the response is to edit civwiki because players on the server like to say delusional a lot

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
+import asyncio
 import json
 from discord import Intents, Embed, File
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
+
 class Bot(commands.Bot):
     def __init__(self, intents):
         super().__init__(command_prefix="%", help_command=None, intents=intents)
+
 
 def load_config():
     with open("config.json") as json_data_file:
         data = json.load(json_data_file)
     return data
 
+
 CONFIG = load_config()
 bot = Bot(intents=Intents.default())
 slash = SlashCommand(bot, sync_commands=True)
 for extension in CONFIG["enabled_cogs"]:
     bot.load_extension("cogs." + extension)
+
+
+@bot.event
+async def on_message(message):
+    if "delusional" in message.content:
+        await message.channel.send("**Edit CivWiki:** https://civwiki.org")
+        await asyncio.sleep(3)
 
 
 @slash.slash(name="mole", description="Mole guy", guild_ids=CONFIG["guild_ids"])
