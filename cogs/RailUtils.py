53f7be56b71ae5f59ae9920d3017af0deb4ccba0
specificlanguage <specificlanguage@gmail.com>
2021-08-07 14:05:57 -0400

    Preliminary A* algorithm
    Not quite done, have to deal with data types. Probably going to switch back to normal json but it might cause a problem with refreshing

--- cogs/RailUtils.py
+++ cogs/RailUtils.py
@@ -1,4 +1,5 @@
 import requests, json
+from math import dist
 from discord.ext import commands
 from discord_slash import cog_ext, SlashCommand, SlashContext
 from discord_slash.utils.manage_commands import create_option
@@ -7,6 +8,41 @@ from discord_slash.utils.manage_commands import create_option
 class RailUtils(commands.Cog, name="RailUtils"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+
+class RailNode:
+    def __init__(self, name: str, x: int, y: int, z: int, aura_edge: list[str], kani_edge:list[str]):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.z = z
+        self.aura_conns = aura_edge
+        self.kani_conns = kani_edge
+
+    def __eq__(self, other):
+        if self.name == other.name and self.x == other.x and self.y == other.y and self.z == other.z:
+            return True
+        return False
+
+    def get_links_kani(self):
+        pass # TODO get links or possibly not create a class for each rail node -- may not be ncessary
+
+
+def get_kani_rail_node(str):
+    with open("resources/kani.json") as kf:
+        kani_json = json.load(kf)
+        try:
+            node = kani_json[str]
+            # railnode = RailNode(name=str,x=node["x"],y=node["y"],z=node["z"], aura_edge=)
+            # TODO: get all links for each system
+        except KeyError:
+            print("Could not find {0}".format(str))
+            return None
+
+
+
+def euclid(start: RailNode, end: RailNode):
+    return dist(start.x - end.x) + dist(start.z - end.z)
 
 
 def get_kani_json():
@@ -23,6 +59,43 @@ def get_aura_json():
     with open("resources/aura.json", "w") as fp:
         fp.close() # clear file before writing again
         json.dump(aura_json, fp)
+
+
+def astar(start: RailNode, end: RailNode):
+    open_list = {start}
+    closed_list = set[RailNode]()
+    path = []
+    gscore = 0
+    fscore = euclid(start, end)  # although we're measuring in blocks, this will give us a better result
+
+    def find_lowest_node(conns: set[RailNode]):
+        cn = None
+        for i in conns:
+            if cn is None or euclid(i, end) < euclid(cn, end):
+                cn = i
+        return cn
+
+    # somewhat pseudocode below
+    while len(open_list) != 0:
+        current_node = find_lowest_node(open_list)
+        open_list.remove(current_node)
+        closed_list.add(current_node)
+
+        if current_node == end:
+            return path
+
+        for link in current_node.get_links_kani():
+            pass
+            # TODO: finish the A* algorithm, after finishing data types earlier
+
+
+
+
+
+
+
+
+
 
 
 
