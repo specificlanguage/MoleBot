84c8732cecd135e958fa014484f95301843babff
specificlanguage <specificlanguage@gmail.com>
2021-08-11 22:45:05 -0400

    Finished AURA integration (needs testing)
    - Added "type" field to AURA nodes to designate seven different types of things
    - Finished working reconstruct rails in AURA
    - Fixed special cases for AURA
    - Fixed very niche equality when setting an AURA line node to the same location as its parent
    - Added special case when the first stop needs to access a line to get onto the system
    - Fixed access point onto AURA routing

--- cogs/AURAHelpers.py
+++
@@ -1,9 +0,0 @@
-def line_routed_direction(cur, prev, nex):
-    start, end = 0, 0
-    for i in range(len(cur.links)):
-        l = cur.links[i]
-        if l == prev:
-            start = i
-        if l == nex:
-            end = i
-    return start, end  # Returns two "strings" of start/end nodes
--- cogs/RailTraverse.py
+++ cogs/RailTraverse.py
@@ -19,11 +19,7 @@ class RailNode:
         self.stop = not (self.station or self.switch)
 
     def __eq__(self, other):
-        if self.name == other.name:
-            return True
-        if self.x == other.x and self.z == other.z:  # just in case
-            return True
-        return False
+        return self.name == other.name
 
     def __str__(self):
         try:
@@ -37,21 +33,22 @@ class RailNode:
         except TypeError:
             return self.name
 
+
 # adaptation for AURA system
 class AuraNode(RailNode):
     def __init__(self, name: str, data: dict):
         super().__init__(name, data)
         self.badlinks = data.get("bad_links", {})
         self.unsafelinks = data.get("unsafe_links", [])
-        self.stop = data.get("stop", False)
-        self.switch = data.get("junction", False)
-        self.stopjunction = data.get("stopjunction", False)
-        self.junctionstop = data.get("junctionstop", False)
-        self.crossing = data.get("crossing", False)
-        self.line = data.get("line", False)
+
+        self.type = data.get("type", "")
+        self.stop = self.type == "stop"
+        self.dest_stop = data.get("dest_stop", "")
         self.dest = data.get("dest", "")
         self.dest_a = data.get("dest_a", "")  # Accounts for AURA line nodes
         self.dest_b = data.get("dest_b", "")
+        self.dest_junction = data.get("dest_junction", "")
+        self.link_dests = data.get("link_dests", {})
 
 
 def euclid(start, end):
@@ -82,12 +79,11 @@ def get_aura_json():
 def reconstruct_path(node: RailNode, start: RailNode):
     path = []
     if node.station or node.switch:
-        path.append(RailNode(name=node.name + ":exit", data={"x": node.x, "z": node.z}))
+        path.append(node.name + ":exit")
     path.append(node.name)
     tot_dist = 0
-    loop = 0
+
     while node != start:
-        print(loop, node)
         tot_dist += (euclid(node, node.parent) + taxi(node, node.parent)) / 2
         if node.name in node.parent.badlinks.keys():
             name = node.name
@@ -107,32 +103,65 @@ def reconstruct_path(node: RailNode, start: RailNode):
         lookup = set()
         path = [x for x in path if x not in lookup and lookup.add(x) is None]
 
-        print(path)
-
     return path[::-1], tot_dist  # need to reverse path
 
 
 # IMPORTANT NOTE: this will return list of strings, not a list of nodes.
 # Currently unable to fix this due to how AURA nodes are structured.
 def reconstruct_aura_path(node: AuraNode, start: AuraNode):
-    path = [node]
+    path = []
     tot_dist = 0
+
+    if node.type == "junctionstop":
+        path.append(node.dest_stop)
 
     # TODO: determine line routed direction
     # TODO: if line, recalculate distance between nodes
+    loop = 0
+    last_node = None
     while node != start:
-        if node.line:
-            pass
         tot_dist += (euclid(node, node.parent) + taxi(node, node.parent)) / 2
+        print(loop, node)
+        loop += 1
+
+        if node.type == "line":
+            start_index = node.links.index(node.parent.name)
+            end_index = node.links.index(last_node.name)
+
+            # Reverse for path
+            if start_index < end_index:
+                path.append(node.dest_b)
+            else:
+                path.append(node.dest_a)
+
+        elif node.type == "stopjunction":
+            path.append(node.dest_junction)
+        elif node.type == "crossing":
+            pass  # no additional dests added
+        elif node.name in node.parent.badlinks.keys():
+            path.append(node.parent.badlinks[node.name])
+        else:
+            path.append(node.dest)
+
+        last_node = node  # in case for lines
         node = node.parent
-        path.append(node)
-    return path[::-1], tot_dist  # need to reverse path
+
+        lookup = set()
+        path = [x for x in path if x not in lookup and lookup.add(x) is None]
+
+    # Special case when the first stop needs to access a line to get on the system
+    if node.link_dests:
+        path.append(node.link_dests[last_node.name])
+
+    return path[::-1], tot_dist
 
 
 # Entry points for AURA/KANI path finding
 def find_kani_route(start: str, end: str):
     start_node = kani_node(start)
     end_node = kani_node(end)
+    if start_node is None or end_node is None:
+        return [], 0
     return astar(start_node, end_node)
 
 
@@ -140,18 +169,17 @@ def find_aura_route(start: str, end: str):
     start_node = aura_node(start)
     end_node = aura_node(end)
 
-    if start_node.switch or end_node.switch or \
-        start_node.crossing or end_node.crossing or \
-        start_node.line or end_node.line:
+    if start_node is None or end_node is None:
+        return [], 0
+
+    incorrect_types = ["switch", "crossing", "junction", "line"]
+    if start_node.type in incorrect_types or end_node.type in incorrect_types:
         return [], -1  # not a valid pair
 
     return astar(start_node, end_node)
 
 
 def astar(start_node: RailNode, end_node: RailNode):
-
-    if start_node is None or end_node is None:
-        return [], 0
 
     open_list = [start_node]
     closed_list = []
@@ -170,14 +198,13 @@ def astar(start_node: RailNode, end_node: RailNode):
         return best_node
 
     while len(open_list) != 0:
-        current_node = find_lowest_node(open_list)
-        print(current_node)
+        current_node = find_lowest_node(open_list)  # find lowest fscore
         open_list.remove(current_node)  # remove current node from open list
         closed_list.append(current_node)  # add to closed list (to prevent backtracking)
 
         # If we've reached our destination no need to continue
         if current_node == end_node:
-            if type(current_node) is AuraNode:
+            if type(current_node) is AuraNode:  # was an AURA workflow
                 return reconstruct_aura_path(current_node, start_node)
             return reconstruct_path(current_node, start_node)
 
@@ -189,7 +216,7 @@ def astar(start_node: RailNode, end_node: RailNode):
         for node in current_node.links:
             if type(current_node) is AuraNode:
                 node = aura_node(node)
-                if node.line:
+                if node.type == "line":
                     # in order to make proper calculations, just have it be the same location.
                     node.x = current_node.x
                     node.z = current_node.z
@@ -199,9 +226,8 @@ def astar(start_node: RailNode, end_node: RailNode):
             # Skip if already in closed/open, or it's not a good link
             if node in closed_list or node in open_list:
                 continue
-            if type(current_node) is AuraNode:
-                if node in current_node.unsafelinks:
-                    continue
+            if type(current_node) is AuraNode and node in current_node.unsafelinks:
+                continue
 
             # Initialize g score if not already
             if current_node.g == 0:
