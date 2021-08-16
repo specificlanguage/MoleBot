f2d047e531056d92c6b7865cb225fb7294cd1afc
specificlanguage <specificlanguage@gmail.com>
2021-08-16 11:37:17 -0400

    Added whereis command functionality
    - Adds command to discord
    - Also links to CivMap for context
    - Adds warning when x or z is greater than 13000
    - Note: may want to add warning for radius greater than 13000 to prevent cases like this

--- cogs/ServerUtils.py
+++ cogs/ServerUtils.py
@@ -1,5 +1,6 @@
 import datetime
 import discord
+from cogs.CivMap import find_closest
 from discord.ext import commands
 from discord_slash import cog_ext, SlashContext
 from discord_slash.utils.manage_commands import create_option
@@ -33,10 +34,48 @@ class ServerUtils(commands.Cog, name="ServerUtils"):
         except:
             await ctx.send("Could not find **" + server_ip + "**, it's either offline or non-existent.")
 
+    @cog_ext.cog_slash(name="whereis",
+                       description="Ping the default server or another server (note this cannot ping Bedrock servers)",
+                       options=[create_option(name="x",
+                                              description="CivClassic x-coordinate",
+                                              option_type=4,
+                                              required=True),
+                                create_option(name="z",
+                                              description="CivClassic z-coordinate",
+                                              option_type=4,
+                                              required=True)])
+    async def whereis(self, ctx: SlashContext, x: int, z: int):
+        if abs(x) > 13000 or abs(z) > 13000:
+            embed = discord.Embed(color=discord.Colour.red())
+            embed.add_field(name="Error!",
+                            value="Value of x or z-coordinate was more than 13000!")
+            await ctx.send(embed=embed)
+            return
+        else:
+            closest = find_closest(x, z)
+            out = ""
+            for item in closest:
+                info = "{0} blocks {1} {2}".format(str(int(item.get("distance"))).rjust(4, " "),
+                                                   item.get("direction").ljust(7),
+                                                   item.get("name"))
+                if item.get("major"):
+                    info = "**" + info + "**"
+                out += info + "\n"
+
+            embed = discord.Embed(color=discord.Colour.gold())
+            embed.add_field(name="Closest features to {0}, {1}:".format(x, z), value=out)
+            embed.add_field(name="CivMap link:",
+                            value="<https://ccmap.github.io/#c=" + str(int(x)) + "," + str(int(z)) + "," + "r400>")
+
+        await ctx.send(embed=embed)
+
+
     # other commands that will become part of this cog:
     # whereis [x] [y] [z] to find a location in the world
     # civmap [x] [y] [z] or civmap[name] to give a link to civmap
     # civwiki to give a link to civwiki
+
+
 
 
 def setup(bot):
