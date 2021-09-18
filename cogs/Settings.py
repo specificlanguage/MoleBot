6471ce9fa5d88acd105a4352e4f7e96eaa3a8ef3
specificlanguage <specificlanguage@gmail.com>
2021-09-18 14:55:46 -0400

    Quick SQL fix
    - ON CONFLICT needed an additional field, uniqueness
    - discord_id is always unique
    - Duplicated logic in /disablemole

--- cogs/Settings.py
+++ cogs/Settings.py
@@ -12,30 +12,15 @@ class Settings(commands.Cog, name="Settings"):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @cog_ext.cog_slash(name="disablemole", description="Disable or reenables the mole guy :(",
-                       options=[create_option(name="setting", description="Optional true/false",
-                                              option_type=5, required=False)])
+    @cog_ext.cog_slash(name="disablemole", description="Disable or reenables the mole guy :(")
     async def disablemole(self, ctx: SlashContext, **setting: bool):
-        """Command handler for /disablemole, superceded task to 'config_mole'"""
-        await self.config_mole(ctx, setting)
+        """Command handler for /disablemole, superceded task to 'modify_mole'"""
+        await modify_mole(ctx)
 
     @cog_ext.cog_subcommand(base="config", name="mole", description="Change /mole behavior")
     async def config_mole(self, ctx: SlashContext):
-        """Command handler for mole subcommand of config, calls subroutines to set mole value.
-        Either disables or enables /mole in this subcommand."""
-        if ctx.guild is None:
-            await ctx.send("This can only be run in a discord.")
-        if ctx.author.guild_permissions.administrator or ctx.author.guild_permissions.manage_messages:
-            setting = change_mole(ctx.guild_id)
-            if setting:
-                await ctx.send("Enabled `/mole` on this server. Do `/disablemole` again to disable it."
-                               .format(ctx.guild.name))
-            else:  # Now set to false
-                await ctx.send("Disabled `/mole` on this server. Do `/disablemole` again to enable it."
-                               .format(ctx.guild.name))
-        else:  # Admins only
-            await ctx.send("Only users with administrator or manage messages permissions can use this command.",
-                           hidden=True)
+        """Command handler for mole subcommand of config, superceded task to 'modify_mole_'."""
+        await modify_mole(ctx)
 
     @cog_ext.cog_subcommand(base="config", name="wiki", description="Change CivWiki query behavior",
                             options=[create_option(name="setting", description="Optional true/false setting",
@@ -62,17 +47,34 @@ class Settings(commands.Cog, name="Settings"):
                            hidden=True)
 
 
+async def modify_mole(ctx: SlashContext):
+    """General handler for mole subcommand of config, calls subroutines to set mole value.
+    Either disables or enables /mole in this subcommand."""
+    if ctx.guild is None:
+        await ctx.send("This can only be run in a discord.")
+    if ctx.author.guild_permissions.administrator or ctx.author.guild_permissions.manage_messages:
+        setting = change_mole(ctx.guild_id)
+        if setting:
+            await ctx.send("Enabled `/mole` on this server. Do `/disablemole` again to disable it."
+                           .format(ctx.guild.name))
+        else:  # Now set to false
+            await ctx.send("Disabled `/mole` on this server. Do `/disablemole` again to enable it."
+                           .format(ctx.guild.name))
+    else:  # Admins only
+        await ctx.send("Only users with administrator or manage messages permissions can use this command.",
+                       hidden=True)
+
 def change_mole(server_id: int):
     """Tells PostgreSQL database to update the mole setting, or just return false if it's new."""
-    set_mole_command = """INSERT INTO settings(discord_id, wiki) VALUES(%s, %s) ON CONFLICT DO
-                            UPDATE SET wiki = %s WHERE discord_id = %s;"""
+    set_mole_command = """INSERT INTO settings(discord_id, mole) VALUES(%s, %s) ON CONFLICT (discord_id) DO
+                            UPDATE SET mole = %s;"""
     with conn.cursor() as curs:
         old_setting = get_mole(server_id)
         if old_setting == "New server":
             return False  # Just to notice that it's disabled as it's new.
             # Mostly for discords that used /disablemole prior to this update
         new_setting = not get_mole(server_id)
-        curs.execute(set_mole_command, [server_id, new_setting, new_setting, server_id])
+        curs.execute(set_mole_command, [server_id, new_setting, new_setting])
         conn.commit()
         return new_setting
 
@@ -85,9 +87,9 @@ def get_mole(server_id: int):
         rows = curs.fetchall()
         if len(rows) == 0:  # The discord doesn't exist in the db yet because it's disabled by default.
             # This case only happens for discords that had /disablemole prior to this update.
-            set_mole_command = """INSERT INTO settings(discord_id, mole) VALUES(%s, %s) ON CONFLICT DO
-                            UPDATE SET wiki = %s WHERE discord_id = %s;"""
-            curs.execute(set_mole_command, [server_id, False, False, server_id])
+            set_mole_command = """INSERT INTO settings(discord_id, mole) VALUES(%s, %s) ON CONFLICT (discord_id) DO
+                            UPDATE SET wiki = %s;"""
+            curs.execute(set_mole_command, [server_id, False, False])
             conn.commit()
             return "New server"
     return rows[0][0]  # Takes the first result, shows the first entry in the row
@@ -101,9 +103,9 @@ def set_wiki_setting(server_id: int, setting: bool):
         if setting is None:
             rows = curs.fetchall()
             setting = not rows[0][0]
-        set_mole_command = """INSERT INTO settings(discord_id, wiki) VALUES(%s, %s) ON CONFLICT DO
-                                UPDATE SET wiki = %s WHERE discord_id = %s;"""
-        curs.execute(set_mole_command, [server_id, setting, setting, server_id])
+        set_mole_command = """INSERT INTO settings(discord_id, wiki) VALUES(%s, %s) ON CONFLICT (discord_id) DO
+                                UPDATE SET wiki = %s;"""
+        curs.execute(set_mole_command, [server_id, setting, setting])
         conn.commit()
     return setting  # Takes the first result, shows the first entry in the row
 
@@ -128,6 +130,7 @@ def init_settings(server_id: int):
         if len(rows) == 0:
             curs.execute("""create table settings(discord_id bigint, mole boolean default false,
             wiki boolean default false);""")
+            curs.execute("""create unique index table_name_discord_id_uindex on settings (discord_id);""")
         curs.execute("""INSERT INTO settings(discord_id) VALUES (%s);""", [server_id])
 
 
